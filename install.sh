19dc21e9d24c58b72db2309283abe62b802741ac
Luca Selinski <lucaselinski@gmail.com>
2022-05-06 16:30:23 +0200

    Improved installation, added uninstall script

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ mkdir -p ~/bin
 cp divera-* ~/bin
 mkdir -p ~/.config/lxsession/LXDE-pi
 cp autostart ~/.config/lxsession/LXDE-pi/autostart
-echo "" >> ~/.bashrc
-echo "# Divera" >> ~/.bashrc
-echo "export DIVERA_MONITOR_URL=\"\"" >> ~/.bashrc
+if [[ -z ${DIVERA_MONITOR_URL} ]];then
+	echo "" >> ~/.bashrc
+	echo "# Divera" >> ~/.bashrc
+	echo "export DIVERA_MONITOR_URL=\"\"" >> ~/.bashrc
+fi
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm ~/.config/lxsession/LXDE-pi/autostart
+rm ~/bin/divera-stop
+rm ~/bin/divera-start
