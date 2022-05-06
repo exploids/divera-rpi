8c27bd97aacc838e6dc8484caca56298aef3527b
Luca Selinski <lucaselinski@gmail.com>
2022-05-06 16:50:48 +0200

    Changed installation

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cp divera-* ~/bin
 mkdir -p ~/.config/lxsession/LXDE-pi
 cp autostart ~/.config/lxsession/LXDE-pi/autostart
 if [[ -z ${DIVERA_MONITOR_URL} ]];then
-	echo "" >> ~/.bashrc
-	echo "# Divera" >> ~/.bashrc
-	echo "export DIVERA_MONITOR_URL=\"\"" >> ~/.bashrc
+	echo "" >> ~/.profile
+	echo "# Divera" >> ~/.profile
+	echo "export DIVERA_MONITOR_URL=\"\"" >> ~/.profile
 fi
